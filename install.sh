9dd5ebe88d834787bccf36faff9a4145441f3b5b
Darkknight1812 <matt.gricius@gmail.com>
2014-08-22 21:21:35 -0700

    Update script with numbers and added timeshift, system monitor, and dropbox

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ while [ "$correct" = "false" ];
 		fi
 	done
 #Load modules for C710
-read -p "Would you like to install touchpad modules? y/n :" answer
+read -p "1. Would you like to install touchpad modules? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Installing touchpad modules..."
 	cat << 'EOF' | sudo tee -a /etc/modules 
@@ -38,7 +38,7 @@ else
 fi
 
 #Configue synaptics for better touchpad; will fix grounding issues on battery
-read -p "Would you like to configure the touchpad settings for better control? y/n :" answer
+read -p "2. Would you like to configure the touchpad settings for better control? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Configuring touchpad at /usr/share/X11/xorg.conf.d/50-synaptics.conf. Backup will be saved."
 	read -p "Press [Enter] to continue:"
@@ -51,7 +51,7 @@ else
 fi
 
 #Configure CrOs flashrom
-read -p "Would you like to install nessecary programs for flashrom? y/n :" answer
+read -p "3. Would you like to install nessecary programs for flashrom? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Installing flashrom dependecies..."
 	sudo apt-get install libftdi-dev libfdt-dev
@@ -67,7 +67,7 @@ else
 fi
 
 #Enable Hibernation
-read -p "Would you like to enable hibernation? y/n :" answer
+read -p "4. Would you like to enable hibernation (Swap must be larger than ram!)? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Enabling hibernation"
 	#Enable hibernation
@@ -96,7 +96,7 @@ else
 fi
 
 #Configure Intel-Pstate
-read -p "Would you like to enable Intel-Pstate? y/n :" answer
+read -p "5. Would you like to enable Intel-Pstate? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Enabling Intel-Pstate. A backup of your current grub configuration will be saved with .bak extension"
 	read -p "Press [Enter] to continue:"
@@ -104,7 +104,7 @@ if [ "$answer" = y ]; then
 	sudo update-grub
 	sudo apt-get install cpufrequtils
 	
-	read -p "Would you like to set the governor to powersaving (this is recommended)? The default is performance. y/n :" answer
+	read -p "Would you like to set the governor to powersave (this is recommended)? The default is performance. y/n :" answer
 	if [ "$answer" = y ]; then
 		echo "Setting governor to powersave"
 		sudo sed -i 's/^GOVERNOR=.*/GOVERNOR="powersave"/' /etc/init.d/cpufrequtils
@@ -116,7 +116,7 @@ else
 fi
 
 #Install thermald and/or tlp
-read -p "Would you like to enable install thermald? y/n :" answer
+read -p "6. Would you like to enable install thermald? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Installing thermald..."
 	sudo apt-get install thermald
@@ -124,7 +124,7 @@ else
 	echo "thermald not installed"
 fi
 
-read -p "Would you like to enable install tlp? y/n :" answer
+read -p "7. Would you like to enable install tlp? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Installing tlp"
 	sudo add-apt-repository ppa:linrunner/tlp
@@ -136,7 +136,7 @@ else
 fi
 
 #Configure keyboard shortcuts
-read -p "Would you like to configure keyboard shortcuts such wifi, brightness, and volume? (manual configuration required) y/n :" answer
+read -p "8. Would you like to configure keyboard shortcuts such wifi, brightness, and volume? (manual configuration required) y/n :" answer
 if [ "$answer" = y ]; then
 	sudo apt-get install xbacklight
 	clear
@@ -161,65 +161,58 @@ EOF
 	clear
 	
 #Install programs
-read -p "Would you like to install htop/restricted-extras/powertop? y/n :" answer
+read -p "9. Would you like to install htop/restricted-extras/powertop/system monitor/synaptic? y/n :" answer
 if [ "$answer" = y ]; then
-	sudo apt-get install htop ubuntu-restricted-extras powertop
+	sudo apt-get install htop ubuntu-restricted-extras powertop indicator-multiload synaptic
 else
 	echo "Programs not installed"
 fi
 
-#Download other programs from the web
-read -p "Would you like to install Chrome Browser and/or indicator sensors (Shows cpu temp)? y/n :" answer
+#Download and install Chrome Browser
+read -p "10. Download and install Chrome Browser? y/n :" answer
 if [ "$answer" = y ]; then
 	cd ~/Downloads
-	read -p "Download and install Chrome Browser? y/n :" answer
-	if [ "$answer" = y ]; then
-		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-		sudo dpkg -i google-chrome-stable_current_amd64.deb
-		#Chrome install will likely fail so force install for dependicies
-		sudo apt-get install -f
-	else
-		echo "Chrome not installed"
-	fi
-
-	read -p "Download and install indicator sensors? y/n :" answer
-	if [ "$answer" = y ]; then
-		wget https://launchpad.net/~alexmurray/+archive/ubuntu/indicator-sensors/+build/5285672/+files/indicator-sensors_0.7-2_amd64.deb
-		sudo dpkg -i indicator-sensors_0.7-2_amd64.deb
-		sudo apt-get install -f
-	else
-		echo "Indicator sensors not install"
-	fi
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	sudo dpkg -i google-chrome-stable_current_amd64.deb
+	#Chrome install will likely fail so force install for dependicies
+	sudo apt-get install -f
 else
-	echo "Programs not installed"
+	echo "Chrome not installed"
 fi
 
-#Install some repos and install their programs
-read -p "Would you like to install Oracle Java 7 and/or My Weather Indicator? y/n :" answer
+#Download and install Alex Murray Indicator Sensors
+read -p "11. Download and install indicator sensors? y/n :" answer
 if [ "$answer" = y ]; then
-	read -p "Install Oracle Java 7? y/n :" answer
-	if [ "$answer" = y ]; then
-		sudo add-apt-repository ppa:webupd8team/java
-		sudo apt-get update
-		sudo apt-get install oracle-java7-installer
-	else
-		echo "Java not installed"
-	fi
-	
-	read -p "Install My Weather Indicator? y/n:" answer
-	if [ "$answer" = y ]; then
-		sudo add-apt-repository ppa:atareao/atareao
-		sudo apt-get update
-		sudo apt-get install my-weather-indicator
-	else
-		echo "Weather Indicator not installed"
-	fi
+	cd ~/Downloads
+	wget https://launchpad.net/~alexmurray/+archive/ubuntu/indicator-sensors/+build/5285672/+files/indicator-sensors_0.7-2_amd64.deb
+	sudo dpkg -i indicator-sensors_0.7-2_amd64.deb
+	sudo apt-get install -f
 else
-	echo "Programs not installed"
+	echo "Indicator sensors not install"
+fi
+
+#Install Oracle Java 7
+read -p "12. Install Oracle Java 7? y/n :" answer
+if [ "$answer" = y ]; then
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo apt-get update
+	sudo apt-get install oracle-java7-installer
+else
+	echo "Java not installed"
+fi
+
+#Install My Weather Indicator	
+read -p "13. Install My Weather Indicator? y/n:" answer
+if [ "$answer" = y ]; then
+	sudo add-apt-repository ppa:atareao/atareao
+	sudo apt-get update
+	sudo apt-get install my-weather-indicator
+else
+	echo "Weather Indicator not installed"
 fi
 
 #Install Playonlinux
-read -p "Would you like to install PlayOnLinux and Wine? y/n :" answer
+read -p "13. Would you like to install PlayOnLinux and Wine? y/n :" answer
 if [ "$answer" = y ]; then
 	wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
 	sudo wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list.d/playonlinux.list
@@ -229,17 +222,8 @@ else
 	echo "PlayOnLinux not install"
 fi
 
-#Upgrade Ubuntu
-read -p "Would you like to upgrade all of the packages for Ubuntu? y/n :" answer
-if [ "$answer" = y ]; then
-	sudo apt-get update
-	sudo apt-get upgrade
-else
-	echo "Packages not upgraded"
-fi
-
 #Install Intel Graphics
-read -p "Would you like to install Intel Graphics Drivers? y/n :"
+read -p "14. Would you like to install Intel Graphics Drivers? y/n :"
 if [ "$answer" = y ]; then
 	cd ~/Downloads
 	wget --no-check-certificate https://download.01.org/gfx/RPM-GPG-KEY-ilg -O - | \
@@ -264,7 +248,7 @@ else
 fi
 
 #Configure system for SSD
-read -p "Do you have a solid state drive and would you like to configure the system for it? y/n :" answer
+read -p "15. Do you have a solid state drive and would you like to configure the system for it? y/n :" answer
 if [ "$answer" = y ]; then
 	echo "Making changes to fstab and creating backup."
 	read -p "Press [Enter] to continue:"
@@ -285,7 +269,7 @@ else
 fi
 
 #Install VirtualBox
-read -p "Would you like to install VirtualBox? y/n :" answer
+read -p "16. Would you like to install VirtualBox? y/n :" answer
 if [ "$answer" = y ]; then
 	sudo apt-get install virtualbox
 else
@@ -293,7 +277,7 @@ else
 fi
 
 #Install XBMC
-read -p "Would you like to install XBMC/KODI? y/n :" answer
+read -p "17. Would you like to install XBMC/KODI? y/n :" answer
 if [ "$answer" = y ]; then
 	sudo apt-get install xbmc
 else
@@ -301,7 +285,7 @@ else
 fi
 
 #Install Pipelight
-read -p "Would you like to install Silverlight/Pipelight? y/n :"
+read -p "18. Would you like to install Silverlight/Pipelight? y/n :"
 if [ "$answer" = y ]; then
 	sudo add-apt-repository ppa:pipelight/stable
 	sudo apt-get update
@@ -310,6 +294,33 @@ if [ "$answer" = y ]; then
 	sudo pipelight-plugin --enable silverlight
 else
 	echo "Silverlight/Pipelight not installed."
+fi
+
+#Install Timeshift
+read -p "19. Would you like to install Timeshift? y/n :" answer
+if [ "$answer" = y ]; then
+	sudo apt-add-repository -y ppa:teejee2008/ppa
+	sudo apt-get update
+	sudo apt-get install timeshift
+else
+	echo "Timeshift not installed."
+fi
+
+#Install Dropbox
+read -p "20. Would you like to install dropbox? y/n :" answer
+if [ "$answer" = y ]; then
+	sudo apt-get install dropbox
+else
+	echo "Dropbox not installed."
+fi
+
+#Upgrade Ubuntu
+read -p "Would you like to upgrade all of the packages for Ubuntu? y/n :" answer
+if [ "$answer" = y ]; then
+	sudo apt-get update
+	sudo apt-get upgrade
+else
+	echo "Packages not upgraded"
 fi
 		
 #Reboot
